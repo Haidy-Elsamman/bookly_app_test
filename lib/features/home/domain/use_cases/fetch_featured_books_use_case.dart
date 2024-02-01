972085d0fec2315e6df48c6c61bb04489fa1cce1
Haidy <haidyahmedelsamman@gmail.com>
2024-02-01 08:12:50 +0200

    + generic use case

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,14 +1,16 @@
 import 'package:bookly_app_test/core/errors/failure.dart';
 import 'package:bookly_app_test/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app_test/features/home/domain/repositories/home_repo.dart';
+import 'package:bookly_app_test/features/home/domain/use_cases/generic_use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase({required this.homeRepo});
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
